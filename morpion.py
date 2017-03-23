2745aa984d7c17b48d161c389f0c082e016fb208
Seth0713 <contact@looma.fr>
2017-03-23 11:20:28 +0100

    Create case for the game
    Remains to add the closing off to the right

--- morpion.py
+++ morpion.py
@@ -1,8 +1,11 @@
 board = []
 
 def create_board(case):
+               
     for x in range(0, case):
-        board.append([";"] * case)
+        board.append([" --"] * case)  
+        board.append(["|  "] * case)
+    board.append([" --"] * case)
         
 def see_board(board):
     for line in board:
