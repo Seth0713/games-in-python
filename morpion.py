09040b650afe006fdc4a867225ff0017a11d395c
Seth0713 <contact@looma.fr>
2017-03-24 14:32:35 +0100

    Adding letters down

--- morpion.py
+++ morpion.py
@@ -1,15 +1,28 @@
 board = []
+letter = [" A"," B"," C"," D"," E"," F"," G"," H"," I"," J"]
+letter2 = []
 
-def create_board(case):               
+number = [1,2,3,4,5,6,7,8,9,10]
+
+def create_board(case):
+     
     for x in range(0, case):
         board.append([" --"] * case)  
-        board.append(["| ."] * case + ["|"])
+        board.append(["|  "] * case + ["|"])
     board.append([" --"] * case)
+
+    for x in range(0, case):
+        letter2.append(letter[x])
     
+        
 def see_board(board):
     for line in board:
         board = (" ").join(line)    
         print (board)
+        
+    showletter = ("  ").join(letter2)
+    print (" "+showletter)
+    return showletter
     return board
 
 nbcase = int(input("How many cases do you want for the game ? : "))
