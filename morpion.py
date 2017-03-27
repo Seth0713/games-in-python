97f571c35146c36839f7947a63776f2b4b7d45d3
Seth0713 <contact@looma.fr>
2017-03-27 14:33:58 +0200

    add object player

--- morpion.py
+++ morpion.py
@@ -1,10 +1,15 @@
+class player(object):
+    def __init__(self, nom, victoire):
+        self.nom = nom
+        self.victoire = victoire
+
 board = []
 letter = [" A"," B"," C"," D"," E"," F"," G"," H"," I"," J"]
 letter2 = []
 number = [1,2,3,4,5,6,7,8,9,10]
 
 def create_board(case):
-     
+ 
     for x in range(0, case):
         board.append([" --"] * case)  
         board.append(["|  "] * case + ["|"])
@@ -28,9 +33,10 @@ def see_board(board):
     print ("\n")
     return board     
 
-nbcase = int(input("How many cases do you want for the game ? : "))
+nbcase = int(input(" How many cases do you want for the game ? : "))
 
 create_board(nbcase)
 
 see_board(board)
     
+
