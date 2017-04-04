08eb56a1bbeb2e7f95c9919ac950dff47140497e
Seth0713 <contact@looma.fr>
2017-04-04 14:19:04 +0200

    Add number for the board and control entered

--- morpion.py
+++ morpion.py
@@ -1,19 +1,19 @@
-class player(object):
+class Player(object):
     def __init__(self, nom, victoire):
         self.nom = nom
         self.victoire = victoire
 
 board = []
-letter = [" A"," B"," C"," D"," E"," F"," G"," H"," I"," J"]
+letter = ["   A"," B"," C"," D"," E"," F"," G"," H"," I"," J"]
 letter2 = []
-number = [1,2,3,4,5,6,7,8,9,10]
+number = ["1","2","3","4","5","6","7","8","9"]
 
 def create_board(case):
  
     for x in range(0, case):
-        board.append([" --"] * case)  
-        board.append(["|  "] * case + ["|"])
-    board.append([" --"] * case)
+        board.append([" "]+[" --"] * case)  
+        board.append([number[x]]+["|  "] * case + ["|"]+[number[x]])
+    board.append([" "]+[" --"] * case)
 
     for x in range(0, case):
         letter2.append(letter[x])    
@@ -31,12 +31,24 @@ def see_board(board):
         print (board)
     showletter()
     print ("\n")
-    return board     
+    return board
 
-nbcase = int(input(" How many cases do you want for the game ? : "))
-
-create_board(nbcase)
-
-see_board(board)
+def choose_case():
+    nbcase = int(input("\n How many cases do you want for the game, choose a number between 1 and 9 ? : "))
+    if nbcase >=1 and nbcase <=9:
+        create_board(nbcase)
+        see_board(board)
+    else:
+        print ("\n Becareful, your entered is not correct")
+        choose_case()
+        
     
+
+player_1 = Player(input(' Player 1 / Enter your name : '),0)
+player_2 = Player(input(' Player 2 / Enter your name : '),0)
+
+choose_case()
+    
+    
+
 
