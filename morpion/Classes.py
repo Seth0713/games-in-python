1587d6420646bf9ce959e15cdfbc81b37961f6eb
Seth0713 <contact@looma.fr>
2017-04-12 14:47:31 +0200

    Add files via upload

---
+++ morpion/Classes.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3
+# -*-coding:UTF-8 -*
+
+class Player(object):
+    def __init__(self, name, victory, defeat, equality):
+        self.name = name
+        self.victory = victory
+        self.defeat = defeat
+        self.equality = equality
+
