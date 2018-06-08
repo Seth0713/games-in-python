c1046f270e76b5a088728998296d89b8906e5190
Seth0713 <contact@looma.fr>
2018-06-08 16:15:27 +0200

    Update roboc.py

--- labyrinth/roboc.py
+++ labyrinth/roboc.py
@@ -2,14 +2,25 @@ print ("Labyrinthes existants :")
 print ("1 - facile.")
 print ("2 - prison.")
 
+check_save = open("cartes/save.txt", "r")
+if check_save is not None:
+    save_ok = input ("\nVous avez une partie en cours, voulez vous la continuer ? (Y or N) :") 
+    if save_ok == 'Y':
+        carte = open("cartes/save.txt", "r")
+        print("\n"+carte.read())
+    else:
+       pass
+                     
 choix = input ("\nEntrez un numéro de labyrinthe pour commencer à jouer : ")
 
 if choix=='1':
     carte = open("cartes/facile.txt", "r")
     print("\n"+carte.read())
 else:
-    carte = open("cartes/prison.txt", "r")   
-    print("\n"+carte.read())
+        carte = open("cartes/prison.txt", "r")   
+        print("\n"+carte.read())
 
 mouvement = input("\n>> ")
     
+    
+
