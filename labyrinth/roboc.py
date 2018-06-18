c7fe2c5a2d20997d2d890e1e01c3381f33d83996
seth0713 <seth0713@protonmail.com>
2018-06-18 16:01:47 +0200

    Move right check

--- labyrinth/roboc.py
+++ labyrinth/roboc.py
@@ -4,6 +4,16 @@ game = "on"
 fichier = []
 countX = 0
 nb = 0
+posOld=" "
+posMem=" "
+
+def Move(nbdep):
+    global posMem
+    posOld=posMem
+    fichier[countX]=posOld
+    posMem = fichier[countX+nbdep]
+    fichier[countX+nbdep]="X"
+    
 
 print ("Labyrinthes existants :")
 print ("1 - facile.")
@@ -20,31 +30,50 @@ if check_save is not None:
                      
 choix = input ("\nEntrez un numéro de labyrinthe pour commencer à jouer : ")
 
+# Récupération de la carte choisie
 if choix=='1':
     carte = "cartes/facile.txt"
 else:
-    carte = "cartes/prison.txt" 
-    
+    carte = "cartes/prison.txt"
 
-while game=="on":
+active_carte = open(carte, "r")
+
+f = active_carte.read()
+
+# Création d'un tableau avec les données de la carte   
+for i in f:
+    fichier.append(i)
     
-    active_carte = open(carte, "r")
-    f = active_carte.read()
-    print (f)
-    for i in f:
-        fichier.append(i)
-        nb+=1
-        if i=="X":
-            countX=nb
-    print (countX) 
+print (("").join(fichier))
+
+# Routine du jeu    
+while game=="on":      
  
     mouvement = input("\n>> ")
 
+# mouvement
+
     if mouvement=="d":
         print ("aller à droite")
-        nb = 0
+
+        for j in fichier:
+            nb+=1
+            if j=="X":
+                countX=nb-1
+        nb=0
+        
+        print (countX)
+        Move(+1)        
+
+        #posOld=posMem
+        #fichier[countX]=posOld
+        #posMem=fichier[countX+1]
+        #fichier[countX+1]="X"
+       
+        print (("").join(fichier))
+        
+# mouvement à gauche
+        
     elif mouvement=="g":
         print ("aller à gauche")
-        nb = 0
-    
-
+        
