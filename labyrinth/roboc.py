494d0318be79f051176708f0c382c18980e75a2c
seth0713 <seth0713@protonmail.com>
2018-06-20 16:27:04 +0200

    Save, O and U

--- labyrinth/roboc.py
+++ labyrinth/roboc.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-game = "on"
+game = ""
 fichier = []
 countX = 0
 nb = 0
@@ -21,7 +21,7 @@ def nbLine():
            fLine=False
 
 def Move(nbdep):
-    global nb, posMem, nbCol, nbMem
+    global nb, posMem, nbCol, nbMem, countX, game
     
     for j in fichier:
         nb+=1
@@ -31,43 +31,70 @@ def Move(nbdep):
     posOld=posMem
     fichier[countX]=posOld
     posMem = fichier[countX+nbdep]
-    fichier[countX+nbdep]="X"
-    print (("").join(fichier))
+
+    print ("posMem",posMem)
+    print("countX",countX)
+    print("posOld",posOld)
+    if posMem!="O":
+        fichier[countX+nbdep]="X"
+        print (("").join(fichier))
+    if posMem=="U":
+        fichier[countX+nbdep]="X"
+        print (("").join(fichier))
+        print ("BRAVO !!!!")
+        game="off"
+        print (game)
     
-print ("Labyrinthes existants :")
+    
+print ("\nLABYRINTHES EXISTANTS :")
 print ("1 - facile.")
 print ("2 - prison.")
 
-#check_save = open("cartes/save.txt", "r")
-#if check_save is not None:
-#    save_ok = input ("\nVous avez une partie en cours, voulez vous la continuer ? (Y or N) : \n>> ") 
-#    if save_ok == 'Y':
-#        carte = open("cartes/save.txt", "r")
-#        print("\n"+carte.read())
-#    else:
-#       pass
-                     
-choix = input ("\nEntrez un numéro de labyrinthe pour commencer à jouer : \n>> ")
+check_save = open("cartes/save.txt", "r")
 
-# Récupération de la carte choisie
-if choix=='1':
-    carte = "cartes/facile.txt"
-elif choix=='2':
-    carte = "cartes/prison.txt"
-else :
-    choix = input ("\nChoix non valide, choisissez la carte 1 ou 2 pour commencer à jouer : \n>> ")
+
+if check_save.read() != "":
+    
+    print (check_save.read())
+    save_ok = input ("\nVous avez une partie en cours, voulez vous la continuer ? (Y or N) : \n>> ")
+
+#else:
+   # choix = input ("\nEntrez un numéro de labyrinthe pour commencer à jouer : \n>> ")
+
+if save_ok == 'Y':
+    carte = "cartes/save.txt"
+    titre = "PARTI SAUVEGARDE"
+else:
+    choix = input ("\nEntrez un numéro de labyrinthe pour commencer à jouer : \n>> ")
+
+    # Récupération de la carte choisie
+    if choix=='1':
+        carte = "cartes/facile.txt"
+        titre = "LABYRINTHES N°1 - FACILE"
+        game="on"
+        
+    elif choix=='2':
+        carte = "cartes/prison.txt"
+        titre = "LABYRINTHES N°2 - PRISON"
+        
+    else :
+        choix = input ("\nChoix non valide, choisissez la carte 1 ou 2 pour commencer à jouer : \n>> ")
+
 
 active_carte = open(carte, "r")
 f = active_carte.read()
 
+
+
 # Création d'un tableau avec les données de la carte   
 for i in f:
     fichier.append(i)  
-print ("dddddddddddddddddddddddddddddddddddddddd\n")
+print (titre,"\n\nBut du jeu : Déplacez X sur l'emplacement U\n")
+print ("Pour vous déplacer utilisez les commandes suivantes :\ng=gauche, d= droite, h=haut, b=bas, q=quitter\n")
 print (("").join(fichier))
 
-
 # Routine du jeu    
+
 while game=="on":      
  
     mouvement = input(">> ")
@@ -75,7 +102,8 @@ while game=="on":
 # mouvement
 
     if mouvement=="d":                   
-        Move(+1)         
+        Move(+1)
+        print (game)
     elif mouvement=="g":
         Move(-1)
     elif mouvement=="h":
